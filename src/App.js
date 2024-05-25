186fce3bbe0d142f555261a148b9a53deb6672fb
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-05-25 21:29:58 +0530

    footer created

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
 import Register from './component/Register';
 import Login from './component/Login';
+import Footer from './component/Footer';
 
 export default function App() {
   return (
     <>
-      <Header/>
-
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      
+      <Footer />
     </>
   )
 }
