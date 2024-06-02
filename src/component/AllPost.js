39fbd14775e063c50dba5a60e5bdf899d491c81b
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-06-02 20:31:17 +0530

    Loader added in home

--- src/component/AllPost.js
+++ src/component/AllPost.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { createTheme, ThemeProvider,CssBaseline, Grid, Container, Typography , Card, CardActionArea, CardContent, CardMedia} from '@mui/material';
+import { createTheme, ThemeProvider, CssBaseline, Grid, Container, Typography, Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
 import { Link } from 'react-router-dom';
 import axios from '../config/Axios';
 import _ from 'lodash'
@@ -29,55 +29,58 @@ export default function AllPost() {
         return content.replace(/<[^>]*>/g, '').substring(0, 200).concat('', '.....')
     }
 
-    if (!posts) {
-        return <Loader/>
-    }
+   
 
-    
-    
+
+
     return (
         <>
             {/* <h2>Home Working</h2> */}
-            <ThemeProvider theme={defaultTheme}>
-                <CssBaseline />
-                <Container maxWidth="lg" sx={{ mt: '30px' }}>
-                    <main>
-                        <Grid container spacing={5} >
-                            {_.shuffle(posts.map((ele) => (
-                                <Grid item xs={12} md={4} key={ele._id}>
-                                    <CardActionArea component={Link} to={`/api/posts/${ele._id}`}>
-                                        <Card sx={{ display: 'flex' , flexDirection:'column' }}>
-                                            <CardMedia
-                                                component="img"
-                                                sx={{ width: '100%', display: { sm: 'block' } }}
-                                                image={ele.bannerImage}
-                                                alt={ele.imageLabel}
-                                            />
-                                            <CardContent sx={{ flex: 1 }}>
-                                                <Typography component="h2" variant="h5">
-                                                    {ele.title}
-                                                </Typography>
-                                                <Typography variant="subtitle" gutterBottom>
-                                                    Author - {`${ele.author.firstName} ${ele.author.lastName}`}
-                                                </Typography>
-                                                <Typography variant="subtitle2" color="text.secondary">
-                                                    {moment(ele.createdAt).format('Do-MMMM-YYYY')}
-                                                </Typography>
-                                                <Typography variant="subtitle1" paragraph>
-                                                    {previewContent(ele.content)}
-                                                </Typography>
-                                                <Typography variant="subtitle1" color="primary">
-                                                    Continue reading...
-                                                </Typography>
-                                            </CardContent>
-                                        </Card>
-                                    </CardActionArea>
+            {!posts ? (<Loader />) : (
+                <>
+                    <ThemeProvider theme={defaultTheme}>
+                        <CssBaseline />
+                        <Container maxWidth="lg" sx={{ mt: '30px' }}>
+                            <main>
+                                <Grid container spacing={5} >
+                                    {_.shuffle(posts.map((ele) => (
+                                        <Grid item xs={12} md={4} key={ele._id}>
+                                            <CardActionArea component={Link} to={`/api/posts/${ele._id}`}>
+                                                <Card sx={{ display: 'flex', flexDirection: 'column' }}>
+                                                    <CardMedia
+                                                        component="img"
+                                                        sx={{ width: '100%', display: { sm: 'block' } }}
+                                                        image={ele.bannerImage}
+                                                        alt={ele.imageLabel}
+                                                    />
+                                                    <CardContent sx={{ flex: 1 }}>
+                                                        <Typography component="h2" variant="h5">
+                                                            {ele.title}
+                                                        </Typography>
+                                                        <Typography variant="subtitle" gutterBottom>
+                                                            Author - {`${ele.author.firstName} ${ele.author.lastName}`}
+                                                        </Typography>
+                                                        <Typography variant="subtitle2" color="text.secondary">
+                                                            {moment(ele.createdAt).format('Do-MMMM-YYYY')}
+                                                        </Typography>
+                                                        <Typography variant="subtitle1" paragraph>
+                                                            {previewContent(ele.content)}
+                                                        </Typography>
+                                                        <Typography variant="subtitle1" color="primary">
+                                                            Continue reading...
+                                                        </Typography>
+                                                    </CardContent>
+                                                </Card>
+                                            </CardActionArea>
+                                        </Grid>
+                                    )))}
                                 </Grid>
-                            )))}
-                        </Grid>
-                    </main>
-                </Container>
-            </ThemeProvider>
+                            </main>
+                        </Container>
+                    </ThemeProvider>
+                </>
+            )}
+
         </>
     )
 }
